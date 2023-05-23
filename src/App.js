3b2dc45de966aee7acbdfeaf1122fcecbfab5477
jvrbrito <britzjames.jbb@gmail.com>
2023-05-23 17:08:35 +0800

    Added basename

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className="App" >
     <Router>
       <Navbar />
-      <Routes>
+      <Routes  basename={process.env.PUBLIC_URL}>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
         <Route path='/item/:id' element={<Details/>}/>
